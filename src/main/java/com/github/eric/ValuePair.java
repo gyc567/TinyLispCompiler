54887a4f7e8199b926701a4acf8a07f599f49b81
eric567 <gyc567@126.com>
2016-07-06 07:12:44 +0800

    add rest and first method of value pair

--- src/main/java/com/github/eric/ValuePair.java
+++ src/main/java/com/github/eric/ValuePair.java
@@ -16,7 +16,22 @@ public class ValuePair {
         this.first = first; this.rest = rest;
     }
 
-    public ValuePair() {
 
+
+
+
+    /** Like Common Lisp first; car of a Pair, or null for anything else. **/
+    public static Object first(Object x) {
+        return (x instanceof ValuePair) ? ((ValuePair)x).first : null;
     }
+
+    /** Like Common Lisp rest; car of a Pair, or null for anything else. **/
+    public static Object rest(Object x) {
+        return (x instanceof ValuePair) ? ((ValuePair)x).rest : null;
+    }
+
+
+
+
+
 }
