d4a8ea01767e064d58a7bfb9a9959d1c356b7d79
eric567 <gyc567@126.com>
2016-07-12 08:06:59 +0800

    add test case for LispCompiler

--- src/test/java/com/github/eric/LispComplierTest.groovy
+++ src/test/java/com/github/eric/LispComplierTest.groovy
@@ -18,7 +18,7 @@ class LispComplierTest extends Specification {
 
     }
 
-    def "Eval"() {
+    def "Eval 1,(+ 1 2 )"() {
         given:"valuepair1 instance with 1,2,valuepair2 with + ,valuepair1"
         ValuePair vp1=new ValuePair(1,2)
         ValuePair vp2=new ValuePair("+",vp1)
@@ -27,6 +27,19 @@ class LispComplierTest extends Specification {
         int rt_int=Integer.valueOf(rt)
         then:"the result shall be 3"
         rt_int==3
+
+    }
+
+    def "Eval 2,(+ 1 2 3)"() {
+        given:"valuepari3 with 3,null valuepair1 instance with 1,2,valuepair2 with + ,valuepair1,valuepari3"
+        ValuePair vp3=new ValuePair(2,3)
+        ValuePair vp1=new ValuePair(1,vp3)
+        ValuePair vp2=new ValuePair("+",vp1)
+        when:"call the mathod eval with valuepair2"
+        Object rt=LispComplier.eval(vp2)
+        int rt_int=Integer.valueOf(rt)
+        then:"the result shall be 3"
+        rt_int==6
 
     }
 }
