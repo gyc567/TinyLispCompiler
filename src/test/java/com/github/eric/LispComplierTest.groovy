09c42244f30ce9d10407eb1ae808b97acc5c8433
eric567 <gyc567@126.com>
2016-07-13 22:44:29 +0800

    update test case for calculate method

--- src/test/java/com/github/eric/LispComplierTest.groovy
+++ src/test/java/com/github/eric/LispComplierTest.groovy
@@ -7,52 +7,67 @@ import spock.lang.Specification
  */
 class LispComplierTest extends Specification {
     def "Caculate for valuepair with 1,2"() {
-        given:"args is 1,2"
-        ValuePair vp1=new ValuePair(1,2)
+        given: "args is 1,2"
+        ValuePair vp1 = new ValuePair(1, 2)
         when: "call calculate method"
 
-        int rt=LispComplier.calculate(0,vp1)
-        then:"the result shall equal 3"
-        rt==3
+        int rt = LispComplier.calculate(0, vp1)
+        then: "the result shall equal 3"
+        rt == 3
 
 
     }
-    def "Caculate for valuepair with 1,2,3"() {
-        given:"args is 1,and 2,3"
 
-        ValuePair vp0=new ValuePair(2,3)
-        ValuePair vp1=new ValuePair(1,vp0)
+    def "Caculate for valuepair with 1,2,3"() {
+
+        given: "args is 1,and 2,3"
+
+
+        ValuePair vp1 = new ValuePair(1, new ValuePair(2, 3))
+
         when: "call calculate method"
 
-        int rt=LispComplier.calculate(0,vp1)
-        then:"the result shall equal 6"
-        rt==6
+        int rt = LispComplier.calculate(0, vp1)
+
+        then: "the result shall equal 6"
+        rt == 6
 
 
     }
 
     def "Eval 1,(+ 1 2 )"() {
-        given:"valuepair1 instance with 1,2,valuepair2 with + ,valuepair1"
-        ValuePair vp1=new ValuePair(1,2)
-        ValuePair vp2=new ValuePair("+",vp1)
-        when:"call the mathod eval with valuepair2"
-        Object rt=LispComplier.eval(vp2)
-        int rt_int=Integer.valueOf(rt)
-        then:"the result shall be 3"
-        rt_int==3
+        given: "valuepair1 instance with 1,2,valuepair2 with + ,valuepair1"
+        ValuePair vp1 = new ValuePair(1, 2)
+        ValuePair vp2 = new ValuePair("+", vp1)
+        when: "call the mathod eval with valuepair2"
+        Object rt = LispComplier.eval(vp2)
+        int rt_int = Integer.valueOf(rt)
+        then: "the result shall be 3"
+        rt_int == 3
+
+    }
+
+    def "Eval (+ 1 9 )"() {
+        given: "valuepair1 instance with 1,2,valuepair2 with + ,valuepair1"
+        ValuePair vp2 = new ValuePair("+", new ValuePair(1, 9))
+        when: "call the mathod eval with valuepair2"
+        Object rt = LispComplier.eval(vp2)
+        int rt_int = Integer.valueOf(rt)
+        then: "the result shall be 3"
+        rt_int == 10
 
     }
 
     def "Eval 2,(+ 1 2 3)"() {
-        given:"valuepari3 with 3,null valuepair1 instance with 1,2,valuepair2 with + ,valuepair1,valuepari3"
-        ValuePair vp3=new ValuePair(2,3)
-        ValuePair vp1=new ValuePair(1,vp3)
-        ValuePair vp2=new ValuePair("+",vp1)
-        when:"call the mathod eval with valuepair2"
-        Object rt=LispComplier.eval(vp2)
-        int rt_int=Integer.valueOf(rt)
-        then:"the result shall be 3"
-        rt_int==6
+        given: "valuepari3 with 3,null valuepair1 instance with 1,2,valuepair2 with + ,valuepair1,valuepari3"
+        ValuePair vp3 = new ValuePair(2, 3)
+        ValuePair vp1 = new ValuePair(1, vp3)
+        ValuePair vp2 = new ValuePair("+", vp1)
+        when: "call the mathod eval with valuepair2"
+        Object rt = LispComplier.eval(vp2)
+        int rt_int = Integer.valueOf(rt)
+        then: "the result shall be 3"
+        rt_int == 6
 
     }
 }
