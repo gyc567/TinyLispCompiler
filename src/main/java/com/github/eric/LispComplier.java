f1a0e8c931e9f5bc99320b29f74e6979311cba94
eric567 <gyc567@126.com>
2016-07-16 09:03:00 +0800

    refactoring with code

--- src/main/java/com/github/eric/LispComplier.java
+++ src/main/java/com/github/eric/LispComplier.java
@@ -5,7 +5,7 @@ package com.github.eric;
  */
 public class LispComplier {
 
-    public static int calculate(int result, Object args) {
+    public static int plus(int result, Object args) {
         if (args == null) {
             return result;
         }
@@ -19,7 +19,24 @@ public class LispComplier {
                 result += ((Integer) first).intValue();
             }
         }
-        return calculate(result, ValuePairUtils.rest(args));
+        return plus(result, ValuePairUtils.rest(args));
+    }
+
+    public static int subtract(int result, Object args) {
+
+            Object first = ValuePairUtils.first(args);
+            Object rest = ValuePairUtils.rest(args);
+            if (first instanceof Integer && rest instanceof Integer) {
+                result = (Integer) first;
+                return plus(result, 0 - (Integer) rest);
+            }else if(first instanceof Integer&&rest instanceof ValuePair)
+            {
+                result=(Integer)first;
+                int sum=plus(0,rest);
+                return plus(result,0-sum);
+            }
+
+        return -1;
     }
 
 
@@ -29,7 +46,7 @@ public class LispComplier {
         if (fn != null && fn.equals("+")) {
             ValuePair args = (ValuePair) valuePair.rest;
 
-            rt = calculate(rt, args);
+            rt = plus(rt, args);
 
 
         }
--- src/test/java/com/github/eric/LispComplierTest.groovy
+++ src/test/java/com/github/eric/LispComplierTest.groovy
@@ -9,12 +9,27 @@ class LispComplierTest extends Specification {
     def "Caculate for valuepair with 1,2"() {
         given: "args is 1,2"
         ValuePair vp1 = new ValuePair(1, 2)
-        when: "call calculate method"
+        when: "call plus method"
 
-        int rt = LispComplier.calculate(0, vp1)
+        int rt = LispComplier.plus(0, vp1)
         then: "the result shall equal 3"
         rt == 3
 
+
+    }
+
+    def "subtract for #vp1,#rtValue"() {
+
+        expect:
+        rtValue == LispComplier.subtract(0, vp1)
+        where:
+        vp1                                                     | rtValue
+        new ValuePair(3, 2)                                     | 1
+        new ValuePair(3, 0)                                     | 3
+        new ValuePair(3, 1)                                     | 2
+        new ValuePair(4, 2)                                     | 2
+        new ValuePair(8, new ValuePair(2, 3))                   | 3
+        new ValuePair(9, new ValuePair(2, new ValuePair(2, 3))) | 2
 
     }
 
@@ -25,9 +40,9 @@ class LispComplierTest extends Specification {
 
         ValuePair vp1 = new ValuePair(1, new ValuePair(2, 3))
 
-        when: "call calculate method"
+        when: "call plus method"
 
-        int rt = LispComplier.calculate(0, vp1)
+        int rt = LispComplier.plus(0, vp1)
 
         then: "the result shall equal 6"
         rt == 6
